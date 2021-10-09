4f8cd0565e7fd26269348b8019f8a5ef52fb3929
SamoKopecky <sano.kopecky@gmail.com>
2021-10-09 22:25:15 +0200

    Init commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using YoutubeDownloader;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var downloader = new Downloader();
+            downloader.DownloadSong("https://www.youtube.com/watch?v=3VGS7rRT8vg");
+        }
+    }
+}
---
+++ YoutubeDownloader/Downloader.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace YoutubeDownloader
+{
+    public class Downloader
+    {
+        public void DownloadSong(string url)
+        {
+            var process = new Process();
+            var youtubeDlName = "";
+            var ffmpegName = "";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                youtubeDlName = "./binaries/linux/youtube-dl";
+                ffmpegName = "./binaries/linux/ffmpeg";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                youtubeDlName = "./binaries/windows/youtube-dl.exe";
+                ffmpegName = "./binaries/windows/ffmpeg.exe";
+            }
+            process.StartInfo.FileName = youtubeDlName;
+            process.StartInfo.Arguments = $"{url} -x --ffmpeg-location {ffmpegName} --audio-format mp3";
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+        }
+    }
+}
